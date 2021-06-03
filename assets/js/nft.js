02f51c8b5656525ae5ef505b356a840ce422c62e
Veerapandi S <veerapandi@rage.fan>
2021-06-03 21:40:43 +0530

    first commit

---
+++ assets/js/nft.js
@@ -0,0 +1,6 @@
+// const db = firebase.frestore();
+// db.collection("tokenDrop").get().then((snapshot) => {
+//     snapshot.docs.forEach(doc => {
+//         console.log("From data",doc.data());
+//     })
+// })
